a8cb63c7dd89915f501ebfb73ba6c2f286103efa
Curt Wilhelm <curt@rightscale.com>
2016-06-08 14:36:10 -0600

    added logging

--- app/models/session.rb
+++ app/models/session.rb
@@ -21,7 +21,11 @@ class Session
 
   def self.create
     begin
+
       config = YAML.load_file("#{Rails.root}/config/vcloudair.yml")[Rails.env]
+      if config["logging"]
+        ENV["VCLOUD_REST_DEBUG_LEVEL"]=config["logging"]
+      end
       connection = VCloudClient::Connection.new(config["host"], config["username"],
       config["password"], config["org"], config["api_version"])
       connection.login
