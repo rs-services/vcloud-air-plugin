36e6e9ea772dcb6ca8902801ec98f670b8605664
Curt Wilhelm <curt@rightscale.com>
2016-06-27 15:05:36 -0600

    add additional attributes for enabling RL10

--- vcloudair-plugin-cat.rb
+++ vcloudair-plugin-cat.rb
@@ -22,19 +22,21 @@ resource "vapp", type: "vcloudair.server" do
   org "TelstraTestvdc001"                   # the vcloudair organization
   name "curt-test-3"                        # the vapp name
   vdc "TelstraTestvdc001"                   # the virtual data center  for the vapp
-  network $network                          # the network to place the vApp
+  network $network                          # the network(s) to place the vApp
   template $os                              # the template to build the vApp
   catalog "Public Catalog"                  # The catalog where to find the template
   description "My vApp from SelfService"    # The description of the vApp
+  cloud "vCloudPOC"                         # name of the UCA cloud
+  server_template "RightScale UCA base"     # ServerTemplate Name
+  deployment @@deployment.name
 end
 
 namespace "vcloudair" do
   service do
-    host "http://1d19b734.ngrok.io" # HTTP endpoint presenting an API defined by self-serviceto act on resources
+    host "http://d33297d9.ngrok.io" # HTTP endpoint presenting an API defined by self-serviceto act on resources
     path "/plugin"  # path prefix for all resources, RightScale account_id substituted in for multi-tenancy
     headers do {
       "user-agent" => "self_service" ,     # special headers as needed
-      "X-Api-Version" => "1.0",
       "X-Api-Shared-Secret" => "Change to a shared secret value"
     } end
   end
@@ -71,12 +73,34 @@ namespace "vcloudair" do
         type "string"
         required true
       end
+      field "cloud" do
+        type "string"
+        required true
+      end
+      field "server_template" do
+        type "string"
+        required true
+      end
+      field "rs_api_refresh_token" do
+        type "string"
+        required false
+      end
+      field "rs_api_host" do
+        type "string"
+        required false
+      end
+      field "deployment" do
+        type "string"
+        required true
+      end
     end
   end
 end
 
 # create new server, and return it's resoruce
 define provision_vapp(@raw_server) return @vapp do
+  call getCredential("RS_API_REFRESH_TOKEN") retrieve $refresh_token
+  call find_shard(@@deployment) retrieve $shard_number
   @vapp = vcloudair.server.create({
     server:{
     org: @raw_server.org,
@@ -85,7 +109,12 @@ define provision_vapp(@raw_server) return @vapp do
     network: @raw_server.network,
     template: @raw_server.template,
     catalog: @raw_server.catalog,
-    description: @raw_server.description
+    description: @raw_server.description,
+    cloud: @raw_server.cloud,
+    deployment: @raw_server.deployment,
+    server_template: @raw_server.server_template,
+    rs_api_refresh_token: $refresh_token,
+    rs_api_host: join(["us-",$shard_number,".rightscale.com"])
   }
    })
    $server_object = to_object(@vapp)
@@ -163,6 +192,8 @@ end
 #########
 # RCL
 #########
+# provision the server
+# return values for output mappings
 define launch_handler(@vapp) return @vapp, $status, $ip do
 
   provision(@vapp)
@@ -173,6 +204,7 @@ define launch_handler(@vapp) return @vapp, $status, $ip do
   call sys_log("server launched",to_s($server))
 end
 
+# stop server from running state
 define do_stop(@vapp) return @vapp,$status, $ip do
   @vapp.stop()
   $server_object = to_object(@vapp)
@@ -181,6 +213,7 @@ define do_stop(@vapp) return @vapp,$status, $ip do
   call sys_log("server stopped",to_s($server))
 end
 
+# start the server from paused/suspended state
 define do_start(@vapp) return @vapp,$status, $ip do
   @vapp.start()
   $server_object = to_object(@vapp)
@@ -189,6 +222,7 @@ define do_start(@vapp) return @vapp,$status, $ip do
   call sys_log("server start",to_s($server))
 end
 
+# power off the server from running state
 define do_power_off(@vapp) return  @vapp,$status,$ip do
   @vapp.power_off()
   $server_object = to_object(@vapp)
@@ -197,6 +231,7 @@ define do_power_off(@vapp) return  @vapp,$status,$ip do
   call sys_log("server powered off",to_s($server))
 end
 
+# power on the server from a stopped state
 define do_power_on(@vapp) return @vapp,$status,$ip do
   @vapp.power_on()
   $server_object = to_object(@vapp)
@@ -205,6 +240,7 @@ define do_power_on(@vapp) return @vapp,$status,$ip do
   call sys_log("server powered on",to_s($server))
 end
 
+# make RS audit_entries
 define sys_log($subject, $detail) do
   rs.audit_entries.create(
     notify: "None",
@@ -214,4 +250,38 @@ define sys_log($subject, $detail) do
       detail: $detail
     }
   )
+end
+
+# get credentials from RS
+define getCredential($cred) return $value do
+  @cred = rs.credentials.get(filter: "name=="+$cred, view: "sensitive")
+  $cred_object = to_object(@cred)
+  $value = first($cred_object["details"])["value"]
+end
+
+# Returns the RightScale shard for the account the given CAT is launched in.
+# It relies on the fact that when a CAT is launched, the resultant deployment description includes a link
+# back to Self-Service.
+# This link is exploited to identify the shard.
+# Of course, this is somewhat dangerous because if the deployment description is changed to remove that link,
+# this code will not work.
+# Similarly, since the deployment description is also based on the CAT description, if the CAT author or publisher
+# puts something like "selfservice-8" in it for some reason, this code will likely get confused.
+# However, for the time being it's fine.
+define find_shard(@deployment) return $shard_number do
+
+  $deployment_description = @deployment.description
+  #rs.audit_entries.create(notify: "None", audit_entry: { auditee_href: @deployment, summary: "deployment description" , detail: $deployment_description})
+
+  # initialize a value
+  $shard_number = "UNKNOWN"
+  foreach $word in split($deployment_description, "/") do
+    if $word =~ "selfservice-"
+      foreach $character in split($word, "") do
+        if $character =~ /[0-9]/
+          $shard_number = $character
+        end
+      end
+    end
+  end
 end
